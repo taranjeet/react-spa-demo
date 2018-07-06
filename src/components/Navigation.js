00a1d04faa26b00ab72f2ffb3ff801afb05c963a
Taranjeet Singh <reachtotj@gmail.com>
2018-07-06 14:31:45 +0530

    add bootstrap to use navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,13 +4,35 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
     return (
-        <nav>
-            <ul>
-                <li><NavLink to='/'>Home</NavLink></li>
-                <li><NavLink to='/about'>About</NavLink></li>
-                <li><NavLink to='/contact'>Contact</NavLink></li>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">React SPA Demo</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                    <NavLink to='/' >Home</NavLink>
+                </a>
+              </li>
+              <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    <NavLink to='/about'>About</NavLink>
+                  </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <NavLink to='/contact'>Contact</NavLink>
+                </a>
+              </li>
             </ul>
+          </div>
         </nav>
+
+
+
+
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
